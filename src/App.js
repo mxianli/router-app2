3f8cb6a689bb9498c6106bd4810aed529cc6db24
JLI <mxianli#hotmail.com>
2020-08-24 06:21:05 -0400

    react router demon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './components/nav';
+import About from './components/about';
+import Shop from './components/shop';
 import './App.css';
+ 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ItemDetail from './components/itemDetail';
+ 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (  
+    <Router>
+    <div className='App' >
+    <Nav />
+    <Switch> 
+    <Route  path='/' exact component= {Home} />
+    <Route  path='/about' component= {About} />
+    <Route  path='/shop' exact component= {Shop} />
+    <Route  path='/shop/:itemId' component= {ItemDetail} />
+    </Switch>
+    
     </div>
+    </Router>
+     
   );
 }
+
+const Home = () =>(
+   <div><h1>HOME</h1></div>
+)
 
 export default App;
